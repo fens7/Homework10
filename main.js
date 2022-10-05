1ff928c2c61f714e12335767ad29167d921a2b9a
Maxym Zavadovych <zavadovich17@gmail.com>
2022-10-05 17:03:24 +0300

    Initial commit

--- main.js
+++ main.js
@@ -24,8 +24,8 @@ let imgArr = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.
 
 const randomNumber = Math.floor(Math.random() * imgArr.length + 1);
 
-imgArr.forEach(() => {
-    imgBlock.innerHTML = `<img style="width: ${window.innerWidth * 0.8}px; height: ${window.innerHeight * 0.8}px"; class="image" src="./images/${randomNumber}.jpg" alt="">`;
-});
+
+imgBlock.innerHTML = `<img style="width: ${window.innerWidth * 0.8}px; height: ${window.innerHeight * 0.8}px"; class="image" src="./images/${randomNumber}.jpg" alt="">`;
+
 
 
